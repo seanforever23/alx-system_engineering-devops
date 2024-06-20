814f9aa6e0cb5a6ca29a5ea8efa402c038769dc7
seanforever23 <seanwanton18@gmail.com>
2024-06-20 23:16:49 +0000

    task 1 corrections

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,20 +2,16 @@
 """Function to print hot posts on a given Reddit subreddit."""
 import requests
 
-
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    url = f"https://oauth.reddit.com/r/{subreddit}/hot"
+    headers = {"User-Agent": "my-reddit-bot/0.1"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+        for post in posts[:10]:
+            print(post["data"]["title"])
+    else:
+        print(None)
